f39faed7609c98e86764575d1c19f2ff3ab47c35
eunje_baek <baekej1015@gamil.com>
2021-02-02 19:59:16 +0900

    init

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <AppHeader v-bind:propsdata="str" v-on:renew="renewStr"></AppHeader>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader'
+
+export default {
+  data: function() { // 다른 컴포넌트들과 충돌하면 안되기 때문에 함수화
+    return {    
+      str : 'Header'
+    }
+  },
+  components: {
+    AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'HELLO';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-form/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">password: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function() {
+      // event.preventDefault(); submit 이벤트의 새로고침을 막는 jquery 기법
+      console.log(this.username, this.password)
+      // 서버 전송
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      const data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
